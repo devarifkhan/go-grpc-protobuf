d871c27fb7f8d67c119df798f172fb497d72c347
devarifkhan <arifcse209@gmail.com>
2025-06-18 13:03:58 +0600

    Add initial implementation of Hello World and HTTP request example

---
+++ basics/import.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello, Go Standard Library!")
+
+	// Make a simple HTTP GET request
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		fmt.Printf("Error fetching data: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Success!")
+	fmt.Println("Response status:", resp.Status)
+	
+	// Read and display response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+	
+	fmt.Printf("Response body:\n%s\n", string(body))
+}
