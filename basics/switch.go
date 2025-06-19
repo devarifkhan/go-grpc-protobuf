5ea148247d5be30faa430adac8f0e6de2e27bb16
devarifkhan <arifcse209@gmail.com>
2025-06-19 15:40:06 +0600

    Add switch.go to demonstrate switch statement usage for day of the week

---
+++ basics/switch.go
@@ -0,0 +1,27 @@
+package main
+
+
+func main() {
+	// This is a simple program that uses a switch statement
+	// to print the day of the week based on a number.
+	day := 3
+
+	switch day {
+	case 1:
+		println("Monday")
+	case 2:
+		println("Tuesday")
+	case 3:
+		println("Wednesday")
+	case 4:
+		println("Thursday")
+	case 5:
+		println("Friday")
+	case 6:
+		println("Saturday")
+	case 7:
+		println("Sunday")
+	default:
+		println("Invalid day")
+	}
+}
