8e63c902ab4f1519af5e29e0d22b3ef2eeb3d349
devarifkhan <arifcse209@gmail.com>
2025-06-19 15:39:16 +0600

    Add if_else.go to implement a number guessing game

---
+++ basics/if_else.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+	
+	// Generate a random number between 1 and 100
+	target := rand.Intn(100) + 1
+	var guess int
+	
+	fmt.Println("Welcome to the Guessing Game!")
+	fmt.Println("I have selected a number between 1 and 100.")
+	
+	// Loop until the user guesses the correct number
+	for {
+		fmt.Print("Enter your guess: ")
+		fmt.Scan(&guess)
+		
+		if guess < target {
+			fmt.Println("Too low! Try again.")
+		} else if guess > target {
+			fmt.Println("Too high! Try again.")
+		} else {
+			fmt.Println("Congratulations! You've guessed the number:", target)
+			break
+		}
+	}
+}
