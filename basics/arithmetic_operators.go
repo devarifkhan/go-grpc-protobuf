f523a3b1ae5027e5327baa2d92e13db6b6443150
devarifkhan <arifcse209@gmail.com>
2025-06-19 15:29:46 +0600

    Add arithmetic_operators.go for basic arithmetic operations

---
+++ basics/arithmetic_operators.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a simple program that uses arithmetic operators
+	// to perform basic calculations.
+	a := 10
+	b := 5
+
+	sum := a + b
+	diff := a - b
+	prod := a * b
+	quot := a / b
+	mod := a % b
+
+	fmt.Println("Sum:", sum)
+	fmt.Println("Difference:", diff)
+	fmt.Println("Product:", prod)
+	fmt.Println("Quotient:", quot)
+	fmt.Println("Modulus:", mod)
+}
