2947b02247a0f5e62b4ce94944561e549e2cd64c
devarifkhan <arifcse209@gmail.com>
2025-06-19 15:28:03 +0600

    Add constants.go with pi and e constants and main function

---
+++ basics/constants.go
@@ -0,0 +1,13 @@
+package main
+
+const pi=3.14
+const e = 2.71828
+
+
+func main() {
+	// This is a simple program that uses a constant
+	// to print the value of pi.
+	println("The value of pi is:", pi)
+	println("The value of e is:", e)
+
+}
