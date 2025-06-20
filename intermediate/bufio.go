59088053a16f8f39146ce9b108250d30d7aee641
devarifkhan <arifcse209@gmail.com>
2025-06-20 14:58:14 +0600

    Add bufio.go and bufio.txt for demonstrating buffered I/O in Go

--- intermediate/bufio.go
+++ intermediate/bufio.go
@@ -1,1 +1,28 @@
 package main
+
+// bufio
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("bufio.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
