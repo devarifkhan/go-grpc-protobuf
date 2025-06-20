1863a2f331582db694b2c93e505b9be7f8497373
devarifkhan <arifcse209@gmail.com>
2025-06-20 15:21:07 +0600

    Add goroutines.go to demonstrate concurrent execution using goroutines and WaitGroup

---
+++ advanced/goroutines.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	// Launch several goroutines
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
+	}
+
+	// Wait for all goroutines to complete
+	wg.Wait()
+
+	fmt.Println("All workers completed")
+}
