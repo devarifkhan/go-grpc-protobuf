910d912c22ccf607eba3884573246938505f1277
devarifkhan <arifcse209@gmail.com>
2025-07-16 11:17:59 +0600

    Add HTTP handlers for /teachers, /students, and /exces routes

--- REST_API_GO/cmd/api/server.go
+++ REST_API_GO/cmd/api/server.go
@@ -14,6 +14,20 @@ func main() {
 		w.Write([]byte("Hello World"))
 		fmt.Println("Hello World Route")
 	})
+	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Teachers"))
+		fmt.Println("Teachers Route")
+	})
+
+	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Students"))
+		fmt.Println("Students Route")
+	})
+
+	http.HandleFunc("/exces", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Exces"))
+		fmt.Println("Exces Route")
+	})
 
 	fmt.Print("Server listening on port ", port)
 	err := http.ListenAndServe(port, nil)
