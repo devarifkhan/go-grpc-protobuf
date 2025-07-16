c3cb79360e9753ff9f405dd42cdb88307140805f
devarifkhan <arifcse209@gmail.com>
2025-07-16 11:31:45 +0600

    Add method-specific handling for /teachers route in HTTP server

--- REST_API_GO/cmd/api/server.go
+++ REST_API_GO/cmd/api/server.go
@@ -15,6 +15,27 @@ func main() {
 		fmt.Println("Hello World Route")
 	})
 	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println(r.Method)
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Hello GET Method on Teachers Route"))
+			fmt.Println("Hello GET Method on Teachers Route")
+		case http.MethodPost:
+			w.Write([]byte("Hello POST Method on Teachers Route"))
+			fmt.Println("Hello POST Method on Teachers Route")
+		case http.MethodPut:
+			w.Write([]byte("Hello PUT Method on Teachers Route"))
+			fmt.Println("Hello PUT Method on Teachers Route")
+		case http.MethodPatch:
+			w.Write([]byte("Hello PATCH Method on Teachers Route"))
+			fmt.Println("Hello PATCH Method on Teachers Route")
+		case http.MethodDelete:
+			w.Write([]byte("Hello DELETE Method on Teachers Route"))
+			fmt.Println("Hello DELETE Method on Teachers Route")
+
+		}
+
 		w.Write([]byte("Teachers"))
 		fmt.Println("Teachers Route")
 	})
