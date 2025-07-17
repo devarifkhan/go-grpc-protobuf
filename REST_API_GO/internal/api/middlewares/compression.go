a00f0c02eb680503040c054fe1dbdff6186f7814
devarifkhan <arifcse209@gmail.com>
2025-07-17 11:52:42 +0600

    Add gzip compression middleware to compress HTTP responses for clients supporting gzip

---
+++ REST_API_GO/internal/api/middlewares/compression.go
@@ -0,0 +1,41 @@
+package middlewares
+
+import (
+	"compress/gzip"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func Compression(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
+			return
+		}
+		// Set the header to indicate that the response is compressed
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer func(gz *gzip.Writer) {
+			err := gz.Close()
+			if err != nil {
+
+			}
+		}(gz)
+
+		// Wrap the ResponseWriter to capture the status code
+		w = &gzipResponseWriter{ResponseWriter: w, Writter: gz}
+
+		next.ServeHTTP(w, r)
+		fmt.Println("Sending response from response time middleware")
+	})
+}
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writter *gzip.Writer
+}
+
+func (g *gzipResponseWriter) Write(b []byte) (int, error) {
+	return g.Writter.Write(b)
+}
